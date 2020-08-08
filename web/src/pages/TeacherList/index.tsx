42833b488d53cf561563052fae80e2e1f1815e66
Charles Hiroshi <charles_hiroshi@outlook.com>
2020-08-08 10:48:17 +0900

    feat: teacher list create

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PageHeader from '../../components/PageHeader'
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 import './styles.css'
 
 function TeacherList() {
@@ -21,6 +22,32 @@ function TeacherList() {
           </div>
         </form>
       </PageHeader>
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img src="https://avatars2.githubusercontent.com/u/7002287?s=460&u=d489a70c068e635a55935c8d210ab1f9fa6365e8&v=4" alt="Charles Hiroshi" />
+            <div>
+              <strong>Charles Hiroshi</strong>
+              <span>Análise de Sistemas</span>
+            </div>
+          </header>
+          <p>
+            Entusiasta das melhores tecnologias para Sistemas avançados.
+            <br /><br />
+            Apaixonado por difundir a experiência de desenvolvimento dos sistemas mais avançados da atualidade.
+          </p>
+          <footer>
+            <p>
+              Preço/Hora
+              <strong>¥1800</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="WhatsApp" />
+                Entrar em contato
+              </button>
+          </footer>
+        </article>
+      </main>
     </div>
   )
 }
